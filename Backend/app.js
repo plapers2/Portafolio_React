2b8f43858b9daea7ae71d54d73decc8f41c126ec
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-17 18:34:49 -0500

    Se añaden rutas a archivo app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 //* Importar rutas
 import contactRoutes from "./src/routes/contact.route.js";
+import projectsRoutes from "./src/routes/project.route.js";
+import skillsRoutes from "./src/routes/skill.route.js";
 // Instancia global
 const app = express();
 
@@ -11,5 +13,7 @@ app.use(express.json());
 
 //* Rutas
 app.use("/api", contactRoutes);
+app.use("/api", projectsRoutes);
+app.use("/api", skillsRoutes);
 
 export default app;
