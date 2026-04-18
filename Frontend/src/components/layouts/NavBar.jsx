02d476c3f68c802432f4cd292fc5fb1ffc8c811e
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-18 13:29:29 -0500

    Se arregla hover y color a Navbar dependiendo de que pagina este, cambia color

--- Frontend/src/components/layouts/NavBar.jsx
+++ Frontend/src/components/layouts/NavBar.jsx
@@ -1,32 +1,43 @@
 import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
   return (
-    <>
-      <nav>
-        <ul className="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
-          <li>
-            <a className="md:p-4 py-3 px-0 block text-green-500 hover:text-green-500" href="/">
-              Inicio
-            </a>
-          </li>
-          <li>
-            <a className="md:p-4 py-3 px-0 block hover:text-green-500" href="/skills">
-              Habilidades
-            </a>
-          </li>
-          <li>
-            <a className="md:p-4 py-3 px-0 block hover:text-green-500" href="/projects">
-              Proyectos
-            </a>
-          </li>
-          <li>
-            <a className="md:p-4 py-3 px-0 block hover:text-green-500" href="/about">
-              Sobre mí
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <ul className="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
+        <li>
+          <NavLink to="/" className={({ isActive }) => `md:p-4 py-3 px-0 block ${isActive ? "text-green-500" : "hover:text-green-500"}`}>
+            Inicio
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/skills"
+            className={({ isActive }) => `md:p-4 py-3 px-0 block ${isActive ? "text-green-500" : "hover:text-green-500"}`}
+          >
+            Habilidades
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) => `md:p-4 py-3 px-0 block ${isActive ? "text-green-500" : "hover:text-green-500"}`}
+          >
+            Proyectos
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => `md:p-4 py-3 px-0 block ${isActive ? "text-green-500" : "hover:text-green-500"}`}
+          >
+            Sobre mí
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
