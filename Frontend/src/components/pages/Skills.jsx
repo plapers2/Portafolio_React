dffd9c719be8a0769b8df6cebc94e5b246612469
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-18 15:35:35 -0500

    Se termina pagina de skills

--- Frontend/src/components/pages/Skills.jsx
+++ Frontend/src/components/pages/Skills.jsx
@@ -1,5 +1,46 @@
+import { NavLink } from "react-router-dom";
+import useFetchApi from "../../hooks/useFetchApi";
 const Skills = () => {
-  return <></>;
+  const { Datos } = useFetchApi("https://portafolio-react-y0p9.onrender.com/api/skills");
+  console.log(Datos);
+  return (
+    <>
+      <section className="mt-20 mb-20 pl-10 flex flex-row">
+        <div className="basis-3/4">
+          <h1 className="text-2xl md:text-4xl xl:text-6xl">
+            <span className="text-green-500 font-serif">Arquitectura,</span> <br />
+            <span className="font-bold font-mono">Código</span> y <span className="text-red-400">Estrategia.</span>
+          </h1>
+        </div>
+      </section>
+      {Datos.map((skill) => {
+        return (
+          <section className="w-full flex bg-black/60 m-2 overflow-x-hidden" key={skill.id}>
+            <div className="flex transition-all duration-300 hover:-translate-y-2 hover:scale-105 hover:shadow-xl">
+              {/* Imagenes */}
+              <div className="basis-1/4 flex flex-col justify-center">
+                <div className="justify-center flex">
+                  <h2 className="text-xl m-6 font-bold">
+                    0{skill.id} - {skill.technology}
+                  </h2>
+                </div>
+                <div className="justify-center flex h-30">
+                  <img src={skill.icon} alt={skill.technology} className="h-full rounded-2xl" />
+                </div>
+              </div>
+              {/* Informacion sobre tecnologia */}
+              <div className="basis-3/4 p-5">
+                <p className="p-3 text-xl md:text-2xl xl:text-4xl">{skill.description}</p>
+                <div className="flex row-auto mt-10">
+                  <p className="w-full p-3 text-sm md:text-xl xl:text-2xl">{skill.long_description}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </>
+  );
 };
 
 export default Skills;
