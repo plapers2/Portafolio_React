afd15ca18c80f58b1fd50addfd9096e5159693f3
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-17 23:22:27 -0500

    Se agregan rutas Projects y Skills

--- Frontend/src/Router/rutas.jsx
+++ Frontend/src/Router/rutas.jsx
@@ -1,6 +1,8 @@
 import { Routes, BrowserRouter, Route } from "react-router-dom";
 import Header from "../components/layouts/Header";
 import Footer from "../components/layouts/Footer";
+import Projects from "../components/pages/Projects";
+import Skills from "../components/pages/Skills";
 const Rutas = () => {
   return (
     <>
@@ -8,8 +10,14 @@ const Rutas = () => {
         <Header />
         <section className="flex flex-wrap flex-row h-auto p-2">
           <article className="flex flex-row flex-wrap justify-evenly">
-            {/* <Projects /> */}
-            {/* <Skills /> */}
+            <Routes>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+              <Route path="/skills">
+                <Skills />
+              </Route>
+            </Routes>
           </article>
         </section>
         <Footer />
