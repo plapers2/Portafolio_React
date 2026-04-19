88c9a735e2da20c4712857d256a7302790655c1f
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-04-18 22:06:30 -0500

    endpointv2

--- Backend/src/routes/project.route.js
+++ Backend/src/routes/project.route.js
@@ -2,5 +2,5 @@ import { Router } from "express";
 import * as projectCtr from "../controllers/project.controller.js";
 const router = Router();
 router.get("/projects", projectCtr.getProject);
-router.put("/projects/:id", projectCtr.putProject);
+router.put("/projects/act/:id", projectCtr.putProject);
 export default router;
